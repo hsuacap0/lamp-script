e554db26533cdd228b8fee64d3476be313f0f11f
hsuacap0 <me@asyraf.my>
2024-04-17 11:41:50 +0800

    add color to output

--- install.sh
+++ install.sh
@@ -1,61 +1,94 @@
 #!/usr/bin/bash
 DEBIAN_FRONTEND=noninteractive
 echo "\$nrconf{restart} = 'a'" >> /etc/needrestart/needrestart.conf
+YELLOW='\033[1;33m'
+NC='\033[0m'
+GREEN='\033[1;32m'
+RED='\033[1;31m'
 user_input() {
-    printf "%s" "Please enter your desired MySQL root crendentials: "
+    printf "${YELLOW}[*]${NC} Please enter your desired MySQL root crendentials:${NC} "
     read -s secret
     echo ""
-    printf "%s" "Please enter the PHP versions that you would like to install: "
+    printf "${YELLOW}[*]${NC} Please enter the PHP versions that you would like to install: "
     read php
 }
 
 system_update() {
-    echo "Starting system update..."
-    sleep 5
-    apt update -y
-    apt upgrade -y 
-    echo ""
+    echo -e "${YELLOW}[*]${NC} Starting system update..."
+    sleep 2
+    apt update -y >/dev/null 2>&1
+    apt upgrade -y >/dev/null 2>&1
+
+    if [ $? -ne 0 ]; then
+        echo -e "${RED}[*]${NC} Error"
+    else
+        echo -e "${GREEN}[*]${NC} OK"
+    fi
 }
 
 nginx () {
-    echo "Installing nginx..."
-    sleep 5
-    add-apt-repository ppa:ondrej/nginx -y
-    apt update -y
-    apt install zip nginx -y
+    echo -e "${YELLOW}[*]${NC} Installing nginx..."
+    sleep 2
+    add-apt-repository ppa:ondrej/nginx -y >/dev/null 2>&1
+    apt update -y  >/dev/null 2>&1
+    apt install zip nginx -y  >/dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        echo -e "${RED}[*]${NC} Error"
+    else
+        echo -e "${GREEN}[*]${NC} OK"
+    fi
 }
 
 php () {
-    echo "Installing PHP Version: $php"
-    sleep 5
-    add-apt-repository ppa:ondrej/php -y
-    apt update -y
-    apt install php$php-fpm php$php-mysql php$php-zip php$php-mbstring php$php-xml php$php-curl php$php-gd php$php-bcmath -y
+    echo -e "${YELLOW}[*]${NC} Installing PHP Version: $php"
+    sleep 2
+    add-apt-repository ppa:ondrej/php -y >/dev/null 2>&1
+    apt update -y >/dev/null 2>&1 
+    apt install php$php-fpm php$php-mysql php$php-zip php$php-mbstring php$php-xml php$php-curl php$php-gd php$php-bcmath -y >/dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        echo -e "${RED}[*]${NC} Error"
+    else
+        echo -e "${GREEN}[*]${NC} OK"
+    fi
 }
 
 certbot () {
-    echo "Installing certbot via Snap..."
-    sleep 5
-    apt install snap -y
-    snap install certbot --classic
+    echo -e "${YELLOW}[*]${NC} Installing certbot via Snap..."
+    sleep 2
+    apt install snap -y >/dev/null 2>&1
+    snap install certbot --classic >/dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        echo -e "${RED}[*]${NC} Error"
+    else
+        echo -e "${GREEN}[*]${NC} OK"
+    fi
 }
 
 mysql_install () {
-    echo "Installing MySQL..."
-    sleep 5
-    apt install mysql-server -y
-    echo "Resetting MySQL root password and disable passwordless login"
-
+    echo -e "${YELLOW}[*]${NC} Installing MySQL..."
+    sleep 2
+    apt install mysql-server -y >/dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        echo -e "${RED}[*]${NC} Error"
+    else
+        echo -e "${GREEN}[*]${NC} OK"
+    fi
 }
 
 mysql_reset () {
-    mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$secret';"
+    echo -e "${YELLOW}[*]${NC} Resetting MySQL root password and disable passwordless login"
+    mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$secret';" >/dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        echo -e "${RED}[*]${NC} Error"
+    else
+        echo -e "${GREEN}[*]${NC} OK"
+    fi
 }
 
 idone () {
-    echo "LAMP Installation completed!"
-    echo "Below are MySQL root crendentials..."
-    echo "Secret: $secret"
+    echo -e "${YELLOW}[*]${NC} LAMP Installation completed!"
+    echo -e "${YELLOW}[*]${NC} Below are MySQL root crendentials..."
+    echo -e "${YELLOW}[*]${NC} Secret: $secret"
 }
 
 user_input
